e3cc8ca2f037cd39bbf61efd2fa862f9ceecd5fa
Luis Tasayco <127450834+LuisTasaycoQuispe@users.noreply.github.com>
2025-06-03 01:36:49 +0000

    actualizacion

--- src/main/java/pe/edu/vallegrande/education/model/Education.java
+++ src/main/java/pe/edu/vallegrande/education/model/Education.java
@@ -1,12 +1,13 @@
 package pe.edu.vallegrande.education.model;
 
-import lombok.Data;
 
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
 
 
 @Data
